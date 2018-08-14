9cf3a7db38c643f3bc099d44f924b387bd1eb8cb
Chris Grivas <chgrivas7@gmail.com>
2018-08-14 20:11:47 +0300

    Initial fix

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,17 +2,25 @@ package com.gildedrose;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GildedRoseTest {
 
+    @Before
+    public void setup() {
+
+    }
+
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals("foo", app.items[0].name);
     }
 
 }
